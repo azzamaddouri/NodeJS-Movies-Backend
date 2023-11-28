343ff856b8ea305ad03d6be23566507c675a4043
azzamaddouri <maddouriazza@gmail.com>
2023-11-28 21:26:24 +0100

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const mongoose=require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 const app=require('./app');
-const port = 3000;
+mongoose.connect(process.env.CONN_STR,{
+    useNewUrlParser:true
+}).then((conn)=>{console.log('DB Connection Successful');})
+const port = process.env.port ||  3000;
 app.listen(port, () => {
     console.log('Server has started');
 });
